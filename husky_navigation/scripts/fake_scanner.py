c88fdb7f5a62ae42574c3bcb3199469746d75ce5
meriel <meriel.k.stein@gmail.com>
2020-07-10 16:28:01 -0400

    added rough terrain gap worlds

--- husky_navigation/scripts/fake_scanner.py
+++ husky_navigation/scripts/fake_scanner.py
@@ -10,7 +10,7 @@ def main():
    rospy.init_node('fake_scan', anonymous=True)
    scan_subscriber = rospy.Subscriber('/remapped_scan', LaserScan, scan_callback)
    scan_publisher = rospy.Publisher('/scan', LaserScan, queue_size=10)
-   rate = rospy.Rate(100)
+   rate = rospy.Rate(3)
    m = 720
    empty_scan_msg = LaserScan()
    empty_scan_msg.angle_min = -2.3561899662
@@ -24,6 +24,15 @@ def main():
    empty_scan_msg.intensities = [float('inf') for i in range(m)]
    h = std_msgs.msg.Header()
    h.frame_id = "base_laser"
+   for i in range(10):
+      h.stamp = rospy.Time.now()
+      h.seq += 1
+      empty_scan_msg.header = h
+      scan_publisher.publish(empty_scan_msg)
+      rate.sleep()
+   #infinite range, zero intensity --> can't detect anyting
+   empty_scan_msg.ranges = [float('inf') for i in range(m)]
+   empty_scan_msg.intensities = [0.0 for i in range(m)]
    while not rospy.is_shutdown():
       h.stamp = rospy.Time.now()
       h.seq += 1
